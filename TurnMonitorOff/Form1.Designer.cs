62fd6fa1086f7c703a685745bb931a5337d54ade
pchsu <science.pchsu@gmail.com>
2019-05-19 00:02:33 +0800

    TurnMonitorOff

---
+++ TurnMonitorOff/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TurnMonitorOff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(13, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "關閉顯示器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(12, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "啟動休眠";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(12, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "關閉休眠";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(442, 84);
+            this.textBox1.TabIndex = 3;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.linkLabel1.Location = new System.Drawing.Point(243, 266);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(198, 19);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "PCHSU\'s Turn Monitor Off";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(13, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(179, 49);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "立即關機";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(13, 134);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 49);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "重新啟動";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(250, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(205, 141);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "平板";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 102);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(205, 198);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "電腦 與 平板";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 310);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "管理軟體 Ver 1.0.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ TurnMonitorOff/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+using System.Runtime.InteropServices;
+using System.Diagnostics; //to DllImport
+
+
+namespace TurnMonitorOff
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+
+        public int WM_SYSCOMMAND = 0x0112;
+        public int SC_MONITORPOWER = 0xF170; 
+        const string subkey = @"System\CurrentControlSet\Control\Power";
+        const int TURN_ON = 1;
+        const int TURN_OFF = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LinkLabel.Link link = new LinkLabel.Link();
+            link.LinkData = "http://pc-hsu.blogspot.tw/2015/09/windows.html";
+            linkLabel1.Links.Add(link);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            get_reg_status();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult = MessageBox.Show("是否關閉顯示器 ? ", "關閉顯示器", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (myResult == DialogResult.Yes)
+            {
+                SendMessage(this.Handle.ToInt32(), WM_SYSCOMMAND, SC_MONITORPOWER, 2);//DLL function
+                LockWorkStation();
+            }
+        }
+
+        private void get_reg_status()
+        {
+            int status = TURN_ON;
+            try
+            {
+                RegistryKey mainKey = Registry.LocalMachine.OpenSubKey(subkey);
+                status = (int)mainKey.GetValue("CsEnabled");
+                if (status == TURN_ON)
+                    this.textBox1.Text = "休眠已啟動 !\r\n若還未生效，請嘗試重新開機。";
+                else
+                    this.textBox1.Text = "休眠已關閉 !\r\n若還未生效，請嘗試重新開機。";
+            }
+            catch (Exception e)
+            {
+                this.textBox1.Text = "發生錯誤，未能取得資訊。";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            DialogResult myResult = MessageBox.Show("是否啟動休眠 ? ", "需要有管理者權限", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (myResult == DialogResult.Yes)
+            {
+                try
+                {
+                    RegistryKey mainKey = Registry.LocalMachine.OpenSubKey(subkey, true);
+                    mainKey.SetValue("CsEnabled", TURN_ON);
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show("操作此功能需要有管理者權限");
+                    return;
+                }
+                get_reg_status();
+                MessageBox.Show("啟動休眠 ! 重新啟動電腦，才會生效");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult = MessageBox.Show("是否關閉休眠 ? ", "需要有管理者權限", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (myResult == DialogResult.Yes)
+            {
+                try
+                {
+                    RegistryKey mainKey = Registry.LocalMachine.OpenSubKey(subkey, true);
+                    mainKey.SetValue("CsEnabled", TURN_OFF);
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show("操作此功能需要有管理者權限");
+                    return;
+                }
+                get_reg_status();
+                MessageBox.Show("關閉休眠 ! 重新啟動電腦，才會生效");
+            }
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData as string);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult = MessageBox.Show("是否關閉電腦 ? ", "關閉電腦", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (myResult == DialogResult.Yes)
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start("shutdown.exe", "/s /t 0");
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show("操作失敗");
+                    return;
+                }
+                MessageBox.Show("關閉電腦 ! 成功 !");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult myResult = MessageBox.Show("是否重啟電腦 ? ", "重啟電腦", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (myResult == DialogResult.Yes)
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start("shutdown.exe", "-r -t 0");
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show("操作失敗");
+                    return;
+                }
+                MessageBox.Show("重新啟動電腦 ! 成功 !");
+            }
+        }
+    }
+}
